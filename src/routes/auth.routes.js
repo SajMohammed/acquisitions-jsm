cdbfced3f53e9a409d5018d8b168caec5ebf3e41
SajMo <mail2sajmo@gmail.com>
2025-10-02 00:42:40 +0400

    add sign-up endpoint and jwt validation

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,9 +1,8 @@
+import { signup } from '#controllers/auth.controller.js';
 import express from 'express';
 const router = express.Router();
 
-router.post('/sign-up', (req, res) => {
-    res.send('POST /api/auth/sign-up response')
-})
+router.post('/sign-up', signup)
 router.post('/sign-in', (req, res) => {
     res.send('POST /api/auth/sign-in response')
 })
